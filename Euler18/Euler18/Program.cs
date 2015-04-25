6e12d0a80c84715cac99cb101673432a5dd06836
garcok <p.gancarz.it@gmail.com>
2015-04-25 18:52:38 +0200

    Metoda główna main.

--- Euler18/Euler18/Program.cs
+++ Euler18/Euler18/Program.cs
@@ -10,6 +10,13 @@ namespace Euler18
     {
         static void Main(string[] args)
         {
+            int numberOfPermutations = 1000000;
+            Permutations permutations = new Permutations();
+
+            string result = permutations.Run(numberOfPermutations);
+
+            Console.WriteLine(string.Format("{0}th {1} {2}", numberOfPermutations, "lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9 is", result));
+            Console.ReadLine();
         }
     }
 }
