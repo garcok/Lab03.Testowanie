de0dfc2861e6f3dd3e5d200e2dd2ee085b48aa90
garcok <p.gancarz.it@gmail.com>
2015-04-25 18:32:00 +0200

    Dodanie testów dla metody Swap
    Testy nie przechodzą.

--- Euler18/Euler18.Tests/PermutationsTests.cs
+++ Euler18/Euler18.Tests/PermutationsTests.cs
@@ -4,12 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Euler18.Tests
 {
     [TestClass]
-    public class PermutationsTests
+    public class PermutationsTests : Permutations
     {
         Permutations permutations = new Permutations();
 
         [TestMethod]
-        public void Run()
+        public void RunTest()
         {
             // Expected values
             string expected1000000 = "2783915460";
@@ -32,6 +32,30 @@ namespace Euler18.Tests
             Assert.AreEqual(expected100, result100, "Error with argument 100");
             Assert.AreEqual(expected250000, result250000, "Error with argument 250000");
 
+        }
+
+        [TestMethod]
+        public void SwapTest()
+        {
+            // Expected values
+            int[] expected1 = new int[] { 4, 2, 1 };
+            int[] expected2 = new int[] { 5, 4, 6, 1, 6 };
+            int[] expected3 = new int[] { 1, 4, 6, 5, 7, 2, 6, 1 };
+
+            // Tables
+            int[] table1 = new int[] { 1, 2, 4 };
+            int[] table2 = new int[] { 4, 5, 6, 1, 6 };
+            int[] table3 = new int[] { 1, 4, 5, 6, 7, 2, 6, 1 };
+           
+            // Method run
+            base.Swap(0, 2, table1);
+            base.Swap(0, 1, table2);
+            base.Swap(2, 3, table3);
+
+            // Asserts
+            CollectionAssert.AreEqual(expected1, table1, "Error with table1");
+            CollectionAssert.AreEqual(expected2, table2, "Error with table2");
+            CollectionAssert.AreEqual(expected3, table3, "Error with table3");
         }
     }
 }
--- Euler18/Euler18/Permutations.cs
+++ Euler18/Euler18/Permutations.cs
@@ -64,7 +64,7 @@ namespace Euler18
         /// <usecase result="45616">GetTable(new int[] {4,5,6,1,6})</usecase>
         /// <usecase result="14567261">GetTable(new int[] {1,4,5,6,7,2,6,1})</usecase>
         /// <returns>Elements of the table</returns>
-        private string GetTable(int[] permutation)
+        protected string GetTable(int[] permutation)
         {
             throw new NotImplementedException();
         }
@@ -78,7 +78,7 @@ namespace Euler18
         /// <usecase table="{5,4,6,1,6}">Swap(0, 1, new int[] {4,5,6,1,6})</usecase>
         /// <usecase table="{1,4,6,5,7,2,6,1}">Swap(2, 3, new int[] {1,4,5,6,7,2,6,1})</usecase>
         /// <param name="table">table</param>
-        private void Swap(int p1, int p2, int[] table)
+        protected void Swap(int p1, int p2, int[] table)
         {
             throw new NotImplementedException();
         }
