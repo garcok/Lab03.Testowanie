dd3842b5c66ad3256c803c183e48ed57f1cc3963
garcok <p.gancarz.it@gmail.com>
2015-04-25 17:36:37 +0200

    Dodanie interfejsu z przypadkami testowymi

---
+++ Euler18/Euler18/IPermutations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler18
+{
+    interface IPermutations
+    {
+        /// <summary>
+        /// Gets solutions of the problem
+        /// </summary>
+        /// <param name="numberOfPermutations">number of permutations</param>
+        /// <usecase result="2783915460">Run(1000000)</usecase>
+        /// <usecase result="0358926471">Run(100000)</usecase>
+        /// <usecase result="0123465789">Run(25)</usecase>
+        /// <usecase result="0123495786">Run(100)</usecase>
+        /// <usecase result="0726348591">Run(250000)</usecase>
+        /// <returns>solution</returns>
+        string Run(int numberOfPermutations);
+    }
+}
